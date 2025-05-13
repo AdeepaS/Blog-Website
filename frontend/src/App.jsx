8ee99acc1a5279a8acfd2b1a417fb74b949735d9
AdeepaS <adeepashashiprabhath@gmail.com>
2025-05-13 11:50:28 +0530

    Implemented the routing among pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,27 +1,38 @@
-import { useState, useEffect } from 'react'
-import { getPost, getPosts, createPost, updatePost, deletePost } from './api'
-import axios from "axios"
-import './App.css'
+import './App.css';
+import {HashRouter as Router, Routes, Route} from "react-router-dom";
+import About from '../pages/About';
+import Home from '../pages/Home';
+import Landing from '../pages/Landing';
+import Profile from '../pages/Profile';
+import ReadBlog from '../pages/ReadBlog';
+import CreateBlog from '../pages/CreateBlog';
+import Contact from '../pages/Contact';
 
 function App() {
   
-  const [posts,setPosts] = useState()
-
-  useEffect(() => {
-    async function loadAllPosts() {
-      let data = await getPosts()
-      if(data){
-        setPosts(data)
-      }
-    }
-
-    loadAllPosts()
-  },[])
+  //Pages
+  //Landing Page
+  //Home page(filtered by recently)
+  //ReadBlog
+  //CreateBlog
+  //Profile
+  //About
+  //Contact
+  
   return (
-    <>
-      {JSON.stringify(posts)}
-    </>
+    <Router>
+      <Routes>
+        <Route path='/' element={<Landing/>}/>
+        <Route path='/home' element={<Home/>}/>
+        <Route path='/createblog' element={<CreateBlog/>}/>
+        <Route path='/readblog' element={<ReadBlog/>}/>
+        <Route path='/profile' element={<Profile/>}/>
+        <Route path='/about' element={<About/>}/>
+        <Route path='/contact' element={<Contact/>}/>
+      </Routes>
+    </Router>
   )
+  
 }
 
 export default App
