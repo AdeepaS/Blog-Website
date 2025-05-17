e2cdde8eff159c582a5d0bcdb912a103e115830a
AdeepaS <adeepashashiprabhath@gmail.com>
2025-05-17 19:50:43 +0530

    created home page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,14 @@
 import './App.css';
 import {HashRouter as Router, Routes, Route} from "react-router-dom";
-import About from '../pages/About';
-import Home from '../pages/Home';
-import Landing from '../pages/Landing';
-import Profile from '../pages/Profile';
-import ReadBlog from '../pages/ReadBlog';
-import CreateBlog from '../pages/CreateBlog';
-import Contact from '../pages/Contact';
-import Navbar from '../components/Navbar';
-import Layout from '../components/Layout';
+import About from './pages/About';
+import Home from './pages/Home';
+import Landing from './pages/Landing';
+import Profile from './pages/Profile';
+import ReadBlog from './pages/ReadBlog';
+import CreateBlog from './pages/CreateBlog';
+import Contact from './pages/Contact';
+import Navbar from './components/Navbar';
+import Layout from './components/Layout'
 
 function App() {
   
---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import { getPosts } from '../api';
+import { useState, useEffect } from 'react';
+
+const Home = () => {
+
+  const [posts, setPosts] = useState([])
+
+  useEffect(()=>{
+    async function loadAllPosts(){
+        const data = await getPosts()
+        setPosts(data)
+    }
+    loadAllPosts()
+  },[])
+
+
+  return (
+    <>
+      {posts.map((post)=>{
+        return(
+          <>
+            <h1>{post.title}</h1>
+            <h2>{post.description}</h2>
+            <p>{post.dateCreated}</p>
+          </>
+        )
+      })}
+    </>
+  )
+}
+
+export default Home;
