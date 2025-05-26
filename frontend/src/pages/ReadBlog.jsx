6e185a8c2bd30e368797f81bf48470e1174a5bc0
AdeepaS <adeepashashiprabhath@gmail.com>
2025-05-26 19:07:08 +0530

    Readblog

--- frontend/src/pages/ReadBlog.jsx
+++ frontend/src/pages/ReadBlog.jsx
@@ -1,8 +1,29 @@
 import React from 'react'
+import {getPost} from "../api"
+import { useParams } from 'react-router-dom'
+import { useState,useEffect } from 'react'
 
 const ReadBlog = () => {
+  
+  const [post, setPost] = useState({})
+
+  let params = useParams() 
+  let id = params.id
+
+  useEffect(()=>{
+    async function loadPost(){
+      let data = await getPost()
+      setPost(data)
+    }
+    loadPost()
+  },[])
   return (
-    <></>
+    <> 
+     <h1>{post.title}</h1> 
+     <h2>{post.description}</h2> 
+     <h3>{post.dateCreated}</h3> 
+     <p>{post.content}</p> 
+    </>
   )
 }
 
